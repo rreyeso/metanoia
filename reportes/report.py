405555b49d785152eec33accd5d8bee05c627241
rreyeso <rcao717@gmail.com>
2022-01-10 00:29:48 -0600

    Table with cyclical indicators

--- reportes/report.py
+++ reportes/report.py
@@ -8,12 +8,13 @@
 # df=pd.DataFrame(result)
 # print(df.head())
 
+from datetime import date, timedelta
 from pathlib import Path
 
 from gnews import GNews
-from newspaper import Article
+from newspaper import Article, Config
 
-google_news = GNews(language='es', country='MX', period='7d', max_results=5)
+from data import cyclical_indicators_table
 
 html_template = """
 <html lang="en">
@@ -22,82 +23,89 @@ html_template = """
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="report.css" rel="stylesheet">
+    <script src="https://cdn.plot.ly/plotly-2.8.3.min.js"></script>
     <title>Document</title>
 </head>
 <body>
 
     <img src="metanoia_logo.png" alt="Metanoia">
     
-    <h1>Monitoreo semanal de cobertura en medios</h1>
+    <h3>Monitoreo semanal de cobertura en medios</h1>
+    <p>Semana del {start_date} al {end_date}</p>
     
-    <h2>Resumen ejecutivo</h2>
+    <h3>Resumen ejecutivo</h2>
     
-    <ul>
+    <div>
         {business_summary}
-    </ul>
+    </div>
 
-    <h2>Indicadores macroeconómicos</h2>
+    <h3>Indicadores macroeconómicos (semáforo)</h2>
     
-    <iframe src="https://public.tableau.com/views/se0_pibe/se0_pib?:embed=y&amp;:display_count=no&amp;:showVizHome=no&amp;:showShareOptions=false&amp;:toolbar=no" width="100%" height="1000" frameborder="0" scrolling="no">
-    </iframe>
+    {semaforo}
     
-    <iframe src="https://public.tableau.com/views/se0_inpc/INPCdeJalisco?:embed=y&amp;:display_count=no&amp;:showVizHome=no&amp;:showShareOptions=false&amp;:toolbar=no" width="100%" height="600" frameborder="0" scrolling="no">
-    </iframe>
-    
-    <!--
-    <iframe src="https://public.tableau.com/shared/W6FNPMB98?:embed=y&amp;:display_count=no&amp;:showVizHome=no&amp;:showShareOptions=false&amp;:toolbar=no" width="1120" height="800" frameborder="0" scrolling="no">
-    </iframe>
-    
-    <iframe src="https://public.tableau.com/views/Patrones_16057339254850/patrones?:embed=y&amp;:display_count=no&amp;:showVizHome=no&amp;:showShareOptions=false&amp;:toolbar=no" width="1120" height="800" frameborder="0" scrolling="no">
-    </iframe>
+    <p class="souces">Fuente: <a href="https://www.inegi.org.mx/app/reloj/semaforo.html">INEGI - Semáforo de componentes cíclicos</a></p>
 
-    <iframe src="https://public.tableau.com/views/ICCJ_t/Dashboard1?:embed=y&amp;:display_count=no&amp;:showVizHome=no&amp;:showShareOptions=false&amp;:toolbar=no" width="1120" height="900" frameborder="0" scrolling="no">
-    </iframe>
-    -->
-    
-    <p class="souces">Fuente: <a href="https://iieg.gob.mx/ns/?page_id=11884">IIEG (Instituto de Información Estadística y Geográfica de Jalisco)</a></p>
-
-    <h2>Actualidad de las industrias restaurantera y porcina</h2>
+    <h3>Actualidad de las industrias restaurantera y porcina</h2>
 
     <div> 
         {specialized_articles}
-    <div>
+    </div>
 </body>
 </html>
 """
 
 def parse_specialized_articles(article):
-    article_obj = Article(article["url"], language="es")
-    article_obj.download()
-    article_obj.parse()
-    article_obj.nlp()
-    html = f"""
-    <section>
-        <h4>{article["title"]}</h4>
-        <p>{article["publisher"]["title"]}, <i>{article["published date"]}</i><p>
-        <p>{article_obj.summary}</p>
-        <p><a href="{article["url"]}">Leer artículo completo.</a></p>
-    </section>
-    """
-    return html
+    try:
+        article_obj = Article(article["url"], language="es")
+        article_obj.download()
+        article_obj.parse()
+        article_obj.nlp()
+        html = f"""
+            <li><b>{article["title"]}</b>. {article_obj.summary}
+            <i><a href="{article["url"]}">{article["publisher"]["title"]}</a>; {article["published date"]}</i></p>
+            </li>
+        """
+        return html
+    except Exception as e:
+        print(e)
+        return ""
 
 def parse_summary_articles(article):
-    article_obj = Article(article["url"], language="es")
-    article_obj.download()
-    article_obj.parse()
-    article_obj.nlp()
-    html = f"""
-    <li>{article["title"]}. {article_obj.summary}. <i><a href="{article["url"]}">{article["publisher"]["title"]}</a>; {article["published date"]}</i></p>
-    </li>
-    """
-    return html
+    try:
+        article_obj = Article(article["url"], language="es")
+        article_obj.download()
+        article_obj.parse()
+        article_obj.nlp()
+        html = f"""
+            <li><b>{article["title"]}</b>. {article_obj.summary}
+            <i><a href="{article["url"]}">{article["publisher"]["title"]}</a>; {article["published date"]}</i></p>
+            </li>
+        """
+        return html    
+    except Exception as e:
+        print(e)
+        return ""
+    
+google_news = GNews(language='es', country='MX', period='8d', max_results=3)
 
-specialized_articles = google_news.get_news('(industria restaurantera) OR (restaurantes AND guadalajara)') \
-    + google_news.get_news('(industria porcina) OR (carne de cerdo)')
+specialized_articles = google_news.get_news('"lupes bbq"') \
+    + google_news.get_news('(industria restaurantera en mexico) OR (restaurantes AND guadalajara)') \
+    + google_news.get_news('(industria porcina) OR (carne de cerdo)') \
+    + google_news.get_news('(uber eats) OR (didi food)')
 specialized_articles = ''.join([parse_specialized_articles(article) for article in specialized_articles])
 
-summary_articles = google_news.get_news('negocios OR politica')
+google_news = GNews(language='es', country='MX', period='8d', max_results=5)
+
+summary_articles = google_news.get_news('economía OR política')
 summary_articles = ''.join([parse_summary_articles(article) for article in summary_articles])
 
+semaforo = cyclical_indicators_table()
+
 with open(Path(__file__).parent / "report.html", "w", encoding="UTF-8") as f:
-    f.write(html_template.format(business_summary=summary_articles, specialized_articles=specialized_articles))
+    f.write(html_template.format(
+        start_date=date.today() - timedelta(days=8),
+        end_date=date.today(),
+        business_summary=summary_articles, 
+        semaforo=semaforo,
+        specialized_articles=specialized_articles),
+    )
